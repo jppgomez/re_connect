93343f719724c904ff16e27215771c659ef48300
jppgomez <77557946+jppgomez@users.noreply.github.com>
2024-07-26 17:39:59 +0100

    website links

--- script/index.js
+++ script/index.js
@@ -3,26 +3,26 @@ let engFlag = document.getElementById("lang_eng");
 let fieldText = document.getElementById("select_lang_text");
 
 ptFlag.addEventListener("mouseenter", () => {
-    ptFlag.style.backgroundImage = "url('re_connect/assets/flags/PT_Inv.svg')";
+    ptFlag.style.backgroundImage = "url('./assets/flags/PT_Inv.svg')";
     fieldText.innerHTML = 'português';
 
     document.getElementById("descript_text").innerHTML = "<b>[nota:]</b> virar o dispositivo horizontalmente e utilizar </br> <i>headphones</i> para uma melhor experiência."
 });
 
 ptFlag.addEventListener("mouseleave", () => {
-    ptFlag.style.backgroundImage = "url('re_connect/assets/flags/PT_Reg.svg')";
+    ptFlag.style.backgroundImage = "url('./assets/flags/PT_Reg.svg')";
     fieldText.innerHTML = '[selecione idioma]';
 });
 
 engFlag.addEventListener("mouseenter", () => {
-    engFlag.style.backgroundImage = "url('re_connect/assets/flags/UK_Inv.svg')";
+    engFlag.style.backgroundImage = "url('./assets/flags/UK_Inv.svg')";
     fieldText.innerHTML = 'english';
 
     document.getElementById("descript_text").innerHTML = "<b>[note:]</b> turn your device horizontally and wear </br> headphones for a better experience."
 });
 
 engFlag.addEventListener("mouseleave", () => {
-    engFlag.style.backgroundImage = "url('re_connect/assets/flags/UK_Reg.svg')";
+    engFlag.style.backgroundImage = "url('./assets/flags/UK_Reg.svg')";
     fieldText.innerHTML = '[select laguage]';
 });
 
